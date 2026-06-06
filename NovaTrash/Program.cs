ef07e17f7475ab851303bbd03ad0c1fe2ecd0c64
enisggc <enisggce@gmail.com>
2026-06-06 17:42:07 +0300

    added frontend

--- NovaTrash/Program.cs
+++ NovaTrash/Program.cs
@@ -36,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 
 app.UseCors("DashboardPolicy");
+app.UseDefaultFiles();
+app.UseStaticFiles(); 
 app.UseAuthorization();
 app.MapControllers();
 
